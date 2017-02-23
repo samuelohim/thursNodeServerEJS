3f0f7601bb8962992f80875f29d73bebb942c95b
Samuel Y <samuel.y@weimeit.com>
2017-02-23 14:19:41 +0800

    ejs template running

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 const eo = require('express');
 app = eo();
+const el = require('express-ejs-layouts');
+
+app.use(eo.static('__dirnam' + '/public'));
+
+app.use(el);
+app.set('view engine', 'ejs');
 port = process.env.PORT || 8080;
 
 app.use(require('./app/routes'));
